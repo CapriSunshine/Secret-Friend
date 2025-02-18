0648606be56fd5a3f474fa2740d2841302e24be6
CapriSun <dyanahz11@gmail.com>
2025-02-17 21:56:08 -0600

    Se añadió el arreglo de amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica 
 // de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friends = [];
+
+function agregarAmigo() {
+    let newFriend = document.getElementById("amigo").value;
+    console.log(newFriend);
+    if (newFriend !="") {
+    friends.push(newFriend);
+    console.log(friends);}
+    else { 
+        alert("Por favor, inserte un nombre");
+    }
+    clean();
+    }
+
+function clean () {
+    document.getElementById("amigo").value = ""; 
+}
