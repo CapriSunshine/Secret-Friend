99be434a025e7db97091ea7cfc0e0dd9fc9ecd12
CapriSun <dyanahz11@gmail.com>
2025-02-19 22:59:22 -0600

    lista en orden inverso

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -13,7 +13,7 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre");
     }
     clean();
-    }
+    }    
 
 function clean () {
     document.getElementById("amigo").value = ""; 
@@ -24,20 +24,19 @@ function clean () {
 // agregar nuevos elementos. Tareas específicas:
 
 //Obtener el elemento de la lista: Utilizar document.getElementById() 
-// o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
-document.getElementById("listaAmigos");
-
+//o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
 
 //Limpiar la lista existente: Establecer lista.innerHTML = "" 
-// para asegurarse de que no haya duplicados al actualizar.
-listaAmigos.innerHTML = "" 
+// para asegurarse de que no haya duplicados al actualizar
 
 //Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos 
 // y crear elementos de lista (<li>) para cada título.
 
-for (let li = 0; li < friends.length; li++) {
-    console.log(friends[li]);
+function lista(){
+    for (let li = friends.length - 1; li >= 0; li--) {
+    document.getElementById("listaAmigos") = friends[li];
+    listaAmigos.innerHTML = friends[li];
+    return;
 }
-
-
+}
 //Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
