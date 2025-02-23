1eba5ae3d5e058c8cf3517ca465371ace375afed
CapriSun <dyanahz11@gmail.com>
2025-02-22 23:32:52 -0600

    se arregló la funcion para añadir amigos y se inicio a crear el aleatorio

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,41 +2,34 @@
 // de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let friends = [];
+listaAmigos.innerHTML = ""
 
-function agregarAmigo() {
+//Funcion que añade amigos con el botón y va añadiendo la lista.
+function addFriend() {
     let newFriend = document.getElementById("amigo").value;
     if (newFriend !="") {
     friends.push(newFriend);
     console.log(friends);
-}
-    else { 
+} else { 
         alert("Por favor, inserte un nombre");
     }
+    listfriends();
     clean();
     }    
-
-function clean () {
-    document.getElementById("amigo").value = ""; 
-}
-
-//Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li>
-// dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de
-// agregar nuevos elementos. Tareas específicas:
-
-//Obtener el elemento de la lista: Utilizar document.getElementById() 
-//o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
-
-//Limpiar la lista existente: Establecer lista.innerHTML = "" 
-// para asegurarse de que no haya duplicados al actualizar
-
-//Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos 
-// y crear elementos de lista (<li>) para cada título.
-
-function lista(){
-    for (let li = friends.length - 1; li >= 0; li--) {
-    document.getElementById("listaAmigos") = friends[li];
-    listaAmigos.innerHTML = friends[li];
+//Funcion que añade nombres a la lista
+function listfriends(){
+    for (let i = friends.length - 1; i >= 0; i--) {
+        let list = document.querySelector("#listaAmigos");
+        list.innerHTML += `<li>${friends[i]}</li>`
     return;
 }
 }
-//Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+
+
+let randomNum = Math.floor(Math.random() * 10);
+console.log(randomNum);  // Número entre 0 y 9
+
+//Funcion que limpia el imput.
+function clean () {
+    document.getElementById("amigo").value = ""; 
+}
