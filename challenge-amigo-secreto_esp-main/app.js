0c20075ac0f29be3eef5e5766e96c18155db798d
CapriSun <dyanahz11@gmail.com>
2025-03-07 23:06:50 -0600

    Se añade submit con enter y cabia color de la interfaz.

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,29 +2,28 @@
 // de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let friends = [];
-listaAmigos.innerHTML = ""
-console.log(friends.lenght);
+listaAmigos.innerHTML = "";
 
 //Funcion que añade amigos con el botón y va añadiendo la lista.
 function addFriend() {
     let newFriend = document.getElementById("amigo").value;
-    if (newFriend !="") {
+    if (newFriend !="" && newFriend !=" ") {
     friends.push(newFriend);
     console.log(friends);
-    console.log(friends.length);
+    listfriends();
 } else { 
         alert("Por favor, inserte un nombre");
     }
-    listfriends();
-    clean();
-    }    
+        clean();
+    }   
+
 //Funcion que añade nombres a la lista
 function listfriends(){
     for (let i = friends.length - 1; i >= 0; i--) {
         let list = document.querySelector("#listaAmigos");
         list.innerHTML += `<li>${friends[i]}</li>`
     return;
-}
+    }
 }
 
 //Funcion que limpia el imput.
@@ -44,5 +43,3 @@ function secretFriend (){
     }
 }
 
-//Falta ver lo de lista inicial vacia y como 
-//evitar se introduscan espacios
